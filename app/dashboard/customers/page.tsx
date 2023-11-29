160aaab9b64402a2ee1496667bd0a5ca18abc426
John Uel Mangaba <johnuelmangaba@gmail.com>
2023-11-30 00:49:32 +0800

    fix displaying of all data available

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
-  return <p>Customer Page</p>;
+import { lusitana } from "@/app/ui/fonts";
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import { Metadata } from "next";
+import { Suspense } from "react";
+import { TableRowSkeleton } from "@/app/ui/skeletons";
+import CustomersTable from "@/app/ui/customers/table";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const customers = await fetchCustomers();
+  const filteredCustomers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <Suspense fallback={<TableRowSkeleton />}>
+        <CustomersTable customers={filteredCustomers} />
+      </Suspense>
+    </div>
+  );
 }
